6cf4f4a46fee1ba49229041f5bc2fb9779577e45
Emmanuel Lepage Vallee <elv1313@gmail.com>
2019-09-03 00:39:24 -0400

    tag: Add `view_next` and `view_previous` to replace `viewnext/viewprev`.
    It use the current naming convention and use the new `view_relative`
    implementation.

--- lib/awful/tag.lua
+++ lib/awful/tag.lua
@@ -1679,11 +1679,40 @@ function tag.viewnext(screen)
     return tag.viewidx(1, screen)
 end
 
+--- View next tag, relative to the currently selected one.
+--
+-- @staticfct awful.tag.view_next
+-- @tparam table args
+-- @tparam[opt=awful.screen.focused] screen args.screen The screen.
+-- @tparam[opt=true] boolean args.rotate When reaching the limit of the array,
+--  start from the other edge.
+-- @tparam[opt=false] boolean args.grouped Use the grouped tag indices instead of
+--  the per screen ones.
+-- @tparam[opt=false] boolean args.force When `grouped` is set, this will untag
+--  the clients tagged on more than 1 tag to allow the tag to be selected.
+function tag.view_next(args)
+    return tag.view_relative(1, args)
+end
+
 --- View previous tag. This is the same a tag.viewidx(-1).
 -- @staticfct awful.tag.viewprev
 -- @param screen The screen.
 function tag.viewprev(screen)
     return tag.viewidx(-1, screen)
+end
+
+--- View previous tag, relative to the currently selected one.
+-- @staticfct awful.tag.viewprev
+-- @tparam table args
+-- @tparam[opt=awful.screen.focused] screen args.screen The screen.
+-- @tparam[opt=true] boolean args.rotate When reaching the limit of the array,
+--  start from the other edge.
+-- @tparam[opt=false] boolean args.grouped Use the grouped tag indices instead of
+--  the per screen ones.
+-- @tparam[opt=false] boolean args.force When `grouped` is set, this will untag
+--  the clients tagged on more than 1 tag to allow the tag to be selected.
+function tag.view_previous(args)
+    return tag.view_relative(-1, args)
 end
 
 --- View only a tag.
